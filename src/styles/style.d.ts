d25b9ad9ea28fd1c50a86897b016d5e7d62924cf
seohee0112 <ghdtj7@naver.com>
2023-04-05 16:36:23 +0900

    style : css 초기화

---
+++ src/styles/style.d.ts
@@ -0,0 +1,41 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      Pic_Color_Coral: string;
+      Pic_Color_Yellow: string;
+      Pic_Color_Blue: string;
+      Pic_Color_Sky: string;
+      Pic_Color_Gray_Black: string;
+      Pic_Color_Gray_2: string;
+      Pic_Color_Gray_3: string;
+      Pic_Color_Gray_4: string;
+      Pic_Color_Gray_5: string;
+      Pic_Color_Gray_6: string;
+      Pic_Color_White: string;
+    };
+    fonts: {
+      Pic_Title1_Pretendard_Bold_24: SerializedStyles;
+      Pic_Title2_Pretendard_SemiBold_20: SerializedStyles;
+      Pic_Title2_Pretendard_SemiBold_20: SerializedStyles;
+      Pic_Title4_Pretendard_Bold_35: SerialzedStyles;
+      Pic_Subtitle1_Pretendard_Semibold_20: SerializedStyles;
+      Pic_Subtitle2_Pretendard_Medium_18: SerializedStyles;
+      Pic_Subtitle3_Pretendard_Semibold_20: SerializedStyles;
+      Pic_Body1_Pretendard_Medium_16: SerializedStyles;
+      Pic_Body2_Pretendard_Bold_16: SerializedStyles;
+      Pic_Caption1_Pretendard_Semibold_12: SerializedStyles;
+      Pic_Caption2_Pretendard_Semibold_14: SerializedStyles;
+      Pic_Noto_B_Title_1: SerializedStyles;
+      Pic_Noto_SB_Title_2: SerializedStyles;
+      Pic_Noto_B_Title_3: SerializedStyles;
+      Pic_Noto_M_Subtitle_1: SerializedStyles;
+      Pic_Noto_SB_Subtitle_2: SerializedStyles;
+      Pic_Noto_M_Subtitle_3: SerializedStyles;
+      Pic_Noto_M_Subtitle_4: SerializedStyles;
+      Pic_Noto_M_Subtitle_5: SerializedStyles;
+      Pic_Noto_SB_Subtitle_6: SerializedStyles;
+    };
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,165 @@
+import { css, DefaultTheme } from "styled-components";
+
+const colors = {
+  Pic_Color_Coral: "#FF5D5D",
+  Pic_Color_Yellow: "#FFFDC2",
+  Pic_Color_Blue: "#3973CB",
+  Pic_Color_Sky: "#C6DDFF",
+  Pic_Color_Gray_Black: "#1E1F21",
+  Pic_Color_Gray_2: "#525C67",
+  Pic_Color_Gray_3: "#8A949E",
+  Pic_Color_Gray_4: "#C3CDD5",
+  Pic_Color_Gray_5: "#E8EBEF",
+  Pic_Color_Gray_6: "#F4F5F7",
+  Pic_Color_White: "#FFFFFF",
+};
+
+const fonts = {
+  Pic_Title1_Pretendard_Bold_24: css`
+    font-family: "PretendardBold", sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 2.4rem;
+    line-height: 2.9rem;
+  `,
+  Pic_Title2_Pretendard_SemiBold_20: css`
+    font-family: "PretendardBold", sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 2rem;
+    line-height: 2.4rem;
+  `,
+  Pic_Title4_Pretendard_Bold_35: css`
+    font-family: "PretendardSemiBold", sans-serif;
+    font-size: 3.5rem;
+    font-weight: 700;
+    font-style: normal;
+    line-height: 4.177rem;
+  `,
+  Pic_Subtitle1_Pretendard_Semibold_20: css`
+    font-family: "PretendardSemiBold", sans-serif;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 2rem;
+    line-height: 2.4rem;
+  `,
+  Pic_Subtitle2_Pretendard_Medium_18: css`
+    font-family: "PretendardMedium", sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 1.8rem;
+    line-height: 2.2rem;
+  `,
+  Pic_Subtitle3_Pretendard_Semibold_20: css`
+    font-family: "PretendardSemiBold", sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 2rem;
+    line-height: 2.148rem;
+  `,
+  Pic_Body1_Pretendard_Medium_16: css`
+    font-family: "PretendardMedium", sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 1.6rem;
+    line-height: 1.9rem;
+  `,
+  Pic_Body2_Pretendard_Bold_16: css`
+    font-family: "PretendardBold", sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 1.6rem;
+    line-height: 1.9rem;
+  `,
+  Pic_Caption1_Pretendard_Semibold_12: css`
+    font-family: "PretendardSemiBold", sans-serif;
+    font-size: 1.2rem;
+    font-weight: 600;
+    font-style: normal;
+    line-height: 1.671rem;
+  `,
+  Pic_Caption2_Pretendard_Semibold_14: css`
+    font-family: "PretendardSemiBold", sans-serif;
+    font-size: 1.4rem;
+    font-weight: 600;
+    font-style: normal;
+    line-height: 1.671rem;
+  `,
+  Pic_Noto_B_Title_1: css`
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 2rem;
+    line-height: 2.7rem;
+    letter-spacing: -0.03em;
+  `,
+  Pic_Noto_SB_Title_2: css`
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1.6rem;
+    line-height: 2.2rem;
+    letter-spacing: -0.02em;
+  `,
+  Pic_Noto_B_Title_3: css`
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 4rem;
+    line-height: 3.95rem;
+  `,
+  Pic_Noto_M_Subtitle_1: css`
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 1.2rem;
+    line-height: 1.6rem;
+    letter-spacing: -0.05em;
+  `,
+  Pic_Noto_SB_Subtitle_2: css`
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1.2rem;
+    line-height: 1.6rem;
+    letter-spacing: -0.02em;
+  `,
+  Pic_Noto_M_Subtitle_3: css`
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 1.6rem;
+    line-height: 2.2rem;
+    letter-spacing: -0.02em;
+  `,
+  Pic_Noto_M_Subtitle_4: css`
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 1.2rem;
+    line-height: 1.6rem;
+    letter-spacing: -0.02em;
+  `,
+  Pic_Noto_M_Subtitle_5: css`
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 1.4rem;
+    line-height: 1.9rem;
+    letter-spacing: -0.05em;
+  `,
+  Pic_Noto_SB_Subtitle_6: css`
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1.4rem;
+    line-height: 1.9rem;
+    letter-spacing: -0.05em;
+  `,
+};
+
+const theme: DefaultTheme = {
+  colors,
+  fonts,
+};
+export default theme;
