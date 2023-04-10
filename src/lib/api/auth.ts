50e814c4fa9e6f96b6ee22a657582da52d2e58c9
seohee0112 <ghdtj7@naver.com>
2023-04-11 01:40:45 +0900

    fix : api 경로 수정

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -2,7 +2,7 @@ import { peerNaClient } from '../axios';
 
 export const getLoginUserInfo = async () => {
   try {
-    const res = await peerNaClient.get(`/oauth2/authorization/github`);
+    const res = await peerNaClient.get(`/api/login`);
     return res;
   } catch (error) {
     console.error(error);
