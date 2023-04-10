3ea9166f484de79c824a68abde5df6ca0258d99c
seohee0112 <ghdtj7@naver.com>
2023-04-11 02:51:00 +0900

    feat : 메인페이지에 헤더 보여주고 router 에 메인페이지 연결

---
+++ src/@components/MainPage/index.tsx
@@ -0,0 +1,12 @@
+import Header from '../@common/Header';
+import { St } from './style';
+
+const MainPage = () => {
+  return (
+    <St.MainWrapper>
+      <Header />
+    </St.MainWrapper>
+  );
+};
+
+export default MainPage;
