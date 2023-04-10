11b995214555bfe27c54ace4004f7edc68356afe
seohee0112 <ghdtj7@naver.com>
2023-04-11 02:49:55 +0900

    style : 헤더 스타일 구현

---
+++ src/@components/@common/Header/style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const St = {
+  HeaderWrapper: styled.header`
+    display: flex;
+    justify-content: space-between;
+
+    width: 100%;
+    height: fit-content;
+    padding: 1rem 3rem;
+
+    background-color: ${({ theme }) => theme.colors.Peer_Color_Sky_1};
+
+    & > svg {
+      width: 7.9rem;
+      height: 7.2rem;
+    }
+  `,
+};
