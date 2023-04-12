8027e53716fbd13ebf5827b54021cc597a400856
seohee0112 <ghdtj7@naver.com>
2023-04-12 17:34:56 +0900

    feat : 콜백 체크

--- src/@components/LoginPage/Callback/index.tsx
+++ src/@components/LoginPage/Callback/index.tsx
@@ -1,5 +1,22 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const Callback = () => {
-  return <div></div>;
+  const code = new URL(window.location.href).searchParams.get('code');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
+  const checkLogin = async () => {
+    console.log(code);
+    if (code) {
+      navigate('/main');
+    }
+    navigate('/');
+  };
+  return null;
 };
 
 export default Callback;
