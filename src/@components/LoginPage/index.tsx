c82378a6bf61ed48fa67dab5bfb5ab193c2fc0d9
seohee0112 <ghdtj7@naver.com>
2023-04-12 09:42:00 +0900

    fix : 경로 프리티어 적용안되게 넣기

--- src/@components/LoginPage/index.tsx
+++ src/@components/LoginPage/index.tsx
@@ -4,17 +4,21 @@ import { getLoginUserInfo } from '../../lib/api/auth';
 import { St } from './style';
 
 const LoginPage = () => {
-  const handleGithubLogin = () => {
-    const data = getLoginUserInfo();
-  };
-  return (
-    <St.LoginPageWrapper>
-      <IcPeerNaLogo />
-      <St.GithubLoginBtn type="button" onClick={handleGithubLogin}>
-        <IcGithub /> <span>Github 계정으로 로그인</span>
-      </St.GithubLoginBtn>
-    </St.LoginPageWrapper>
-  );
+	const handleGithubLogin = () => {
+		// const data = getLoginUserInfo();
+		const URL = `${process.env.REACT_APP_IP}/oauth2/authorize/github?redirect_uri='${process.env.REACT_APP_IP}/callback'`;
+
+		window.open(URL);
+	};
+	return (
+		<St.LoginPageWrapper>
+			<IcPeerNaLogo />
+			<St.GithubLoginBtn type="button" onClick={handleGithubLogin}>
+				<IcGithub />
+				<span>Github 계정으로 로그인</span>
+			</St.GithubLoginBtn>
+		</St.LoginPageWrapper>
+	);
 };
 
 export default LoginPage;
