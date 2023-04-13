8739e942abd3e0df0f17ca9f3cbaa046716b101d
seohee0112 <ghdtj7@naver.com>
2023-04-13 16:59:57 +0900

    style : 주제 선택하기 스타일링

---
+++ src/@components/MainPage/LearningExperience/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const St = {
+  LearningExperienceSectionr: styled.section`
+    display: flex;
+    flex-direction: column;
+    row-gap: 5.5rem;
+
+    width: 100%;
+    margin-bottom: 5.4rem;
+  `,
+};
