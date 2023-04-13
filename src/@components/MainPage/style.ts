4533dd51ac5a0b3600c20bd5106d5d9ae7ee3f35
seohee0112 <ghdtj7@naver.com>
2023-04-13 17:04:58 +0900

    style : 메인페이지 좌측 section 스타일링

--- src/@components/MainPage/style.ts
+++ src/@components/MainPage/style.ts
@@ -3,5 +3,16 @@ import styled from 'styled-components';
 export const St = {
   MainWrapper: styled.div`
     display: flex;
+    flex-direction: column;
+
+    width: 100%;
+
+    & > section {
+      width: 20%;
+      margin-top: 2.2rem;
+      margin-left: 2%;
+
+      margin-bottom: 4rem;
+    }
   `,
 };
