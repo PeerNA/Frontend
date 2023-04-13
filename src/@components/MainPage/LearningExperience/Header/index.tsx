bc71dc6bae58c206b22e2f934de489266689ce13
seohee0112 <ghdtj7@naver.com>
2023-04-13 17:00:58 +0900

    feat : 학습경력 헤더 컴포넌트 구현

---
+++ src/@components/MainPage/LearningExperience/Header/index.tsx
@@ -0,0 +1,13 @@
+import { St } from './style';
+import { IcOnlineLearning } from '../../../../assets/icon';
+
+const Header = () => {
+  return (
+    <St.Header>
+      <IcOnlineLearning />
+      <h1>학습경력</h1>
+    </St.Header>
+  );
+};
+
+export default Header;
