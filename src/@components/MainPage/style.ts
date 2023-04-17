f5acb4653659fc35ddb58a79ab117364ceafbae4
seohee0112 <ghdtj7@naver.com>
2023-04-17 16:27:36 +0900

    style : 학습 경력 , 학습 기록 width 비율 조정

--- src/@components/MainPage/style.ts
+++ src/@components/MainPage/style.ts
@@ -3,16 +3,19 @@ import styled from 'styled-components';
 export const St = {
   MainWrapper: styled.div`
     display: flex;
-    flex-direction: column;
 
     width: 100%;
 
     & > section {
-      width: 20%;
-      margin-top: 2.2rem;
-      margin-left: 2%;
+      margin: 2%;
 
       margin-bottom: 4rem;
+      &:first-child {
+        width: 20%;
+      }
+      &:last-child {
+        width: 80%;
+      }
     }
   `,
 };
