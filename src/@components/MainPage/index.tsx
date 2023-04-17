faca85da8e14ec96cde2f4b0701df1763d81faa2
seohee0112 <ghdtj7@naver.com>
2023-04-17 16:40:55 +0900

    feat : 메인 페이지에 필요한 모든 컴포넌트 추가

--- src/@components/MainPage/index.tsx
+++ src/@components/MainPage/index.tsx
@@ -1,19 +1,26 @@
 import PeerNaHeader from '../@common/PeerNaHeader';
 import LearningExperience from './LearningExperience';
-import Header from './LearningExperience/Header';
+import LearningRecord from './LearningRecord';
+import Header from './LearningRecord/Header';
 import PeerMatchingBtn from './PeerMatchingBtn';
 import { St } from './style';
 
 const MainPage = () => {
   return (
-    <St.MainWrapper>
+    <>
       <PeerNaHeader />
-      <section>
-        <Header />
-        <LearningExperience />
-        <PeerMatchingBtn />
-      </section>
-    </St.MainWrapper>
+
+      <St.MainWrapper>
+        <section>
+          <LearningExperience />
+          <PeerMatchingBtn />
+        </section>
+        <section>
+          <Header />
+          <LearningRecord />
+        </section>
+      </St.MainWrapper>
+    </>
   );
 };
 
