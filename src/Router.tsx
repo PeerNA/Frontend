33b481c03a39dc49d34b1b4ecccd656c5f527e2b
seohee0112 <ghdtj7@naver.com>
2023-04-20 18:53:49 +0900

    feat : 학습 기록 상세보기 페이지 라우터 연결

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
@@ -12,12 +12,14 @@ const Router = () => {
   return (
     <BrowserRouter>
       <RecoilRoot>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/callback" element={<CallBack />} />
-          <Route path="/main" element={<MainPage />} />
-          <Route path="/detail/:historyid" element={<LearningDetailPage />} />
-        </Routes>
+        <Suspense>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/callback" element={<CallBack />} />
+            <Route path="/main" element={<MainPage />} />
+            <Route path="/detail/:historyId" element={<LearningDetailPage />} />
+          </Routes>
+        </Suspense>
       </RecoilRoot>
     </BrowserRouter>
   );
