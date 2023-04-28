76474edfb20425d4b6f9d0d2f285beb20216d592
seohee0112 <ghdtj7@naver.com>
2023-04-28 13:29:33 +0900

    feat : 커스텀 훅으로 다른 사람 답변 가져오기 구현

---
+++ src/lib/hooks/useGetAnswerList.tsx
@@ -0,0 +1,29 @@
+import useSWRInfinite from 'swr/infinite';
+import { GetLearningHistoryInfo } from '../../type/history';
+
+import { peerNaGetFetcher } from '../axios';
+
+const useGetAnswerList = (problemId: number) => {
+  const { data, isLoading, error, size, setSize } = useSWRInfinite<GetLearningHistoryInfo[]>(
+    (idx: number, answerList: GetLearningHistoryInfo[]) => {
+      console.log('idx', idx);
+      if (!idx) return `api/problems/replies?problemId=${problemId}`;
+      if (answerList[0]) return `api/problems/replies?problemId=${problemId}&page=${idx}`;
+      return null;
+    },
+    peerNaGetFetcher,
+    {
+      errorRetryCount: 1,
+    },
+  );
+  const parseResultList = data?.flat();
+  return {
+    answerList: parseResultList,
+    isLoading,
+    isError: error,
+    size,
+    setSize,
+  };
+};
+
+export default useGetAnswerList;
