f4a39dd6d4e38ff4cd953f35518d7190166fe6a1
seohee0112 <ghdtj7@naver.com>
2023-04-28 13:30:56 +0900

    feat : 학습기록 상세조회 컴포넌트 구현

--- src/lib/hooks/useGetLearningDetail.tsx
+++ src/lib/hooks/useGetLearningDetail.tsx
@@ -4,8 +4,7 @@ import { peerNaClient } from '../axios';
 import useSWR from 'swr';
 
 const useGetLearningDetail = (historyId: number) => {
-  const { data, error } = useSWR<AxiosResponse<GetHistoryDetailInfo>>(`/vote/common/${historyId}`, peerNaClient, {
-    // 실패시 재요청 3번
+  const { data, error } = useSWR<AxiosResponse<GetHistoryDetailInfo>>(`api/detail-history/${historyId}`, peerNaClient, {
     errorRetryCount: 3,
   });
   return {
