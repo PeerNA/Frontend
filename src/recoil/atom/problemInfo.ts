872b09df1e685905e4d220fead302cfbb4944e4e
seohee0112 <ghdtj7@naver.com>
2023-04-28 13:19:13 +0900

    feat : 문제 관련 타입 추가 및 atom 선언

--- src/recoil/atom/problemInfo.ts
+++ src/recoil/atom/problemInfo.ts
@@ -1,4 +1,18 @@
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
+import { ProblemInfo } from '../../type/problem';
+
+enum StateType {
+  PROBLEM_INFO = 'problemInfo',
+  ANSWER_INFO_MODAL = 'answerInfo',
+}
 
 const { persistAtom } = recoilPersist();
+
+export const problemInfoState = atom<ProblemInfo>({
+  key: StateType.PROBLEM_INFO,
+  default: {
+    problemId: 0,
+  },
+  effects_UNSTABLE: [persistAtom],
+});
--- src/type/problem.ts
+++ src/type/problem.ts
@@ -1,3 +1,9 @@
-export interface HashTagList {
-  hashTagList: string[];
+export interface HashTagInfo {
+  keywordList: string[];
+}
+export interface ProblemInfo {
+  problemId: number;
+}
+export interface GetExampleAnswer {
+  answer: string;
 }
