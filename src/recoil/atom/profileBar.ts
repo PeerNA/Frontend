6f58a0e2240600e7c94ebc4ab62e6e7b1328b17b
seohee0112 <ghdtj7@naver.com>
2023-04-28 13:27:50 +0900

    fix : 새로고침 해도 유지하는 속성추가

--- src/recoil/atom/profileBar.ts
+++ src/recoil/atom/profileBar.ts
@@ -1,10 +1,12 @@
-import { ActiveModalInfo, AnswerModalInfo } from './../../type/userInfo';
+import { ActiveModalInfo, UserAnswerInfo } from './../../type/userInfo';
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 
 enum StateType {
   ACTIVE_STATE_MODAL = 'activestatemodal',
   ANSWER_INFO_MODAL = 'answerInfo',
 }
+const { persistAtom } = recoilPersist();
 
 export const modalInfoState = atom<ActiveModalInfo>({
   key: StateType.ACTIVE_STATE_MODAL,
@@ -12,13 +14,15 @@ export const modalInfoState = atom<ActiveModalInfo>({
     isPeernaModal: false,
     isProfileModal: false,
   },
+  effects_UNSTABLE: [persistAtom],
 });
 
-export const answerInfoState = atom<AnswerModalInfo>({
+export const answerInfoState = atom<UserAnswerInfo>({
   key: StateType.ANSWER_INFO_MODAL,
   default: {
     userName: '',
     imageUrl: '',
     answer: '',
   },
+  effects_UNSTABLE: [persistAtom],
 });
