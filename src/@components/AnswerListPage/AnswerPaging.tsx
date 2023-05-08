2f10a48f34ec6180a6ee0e88610a0b40f1830518
seohee0112 <ghdtj7@naver.com>
2023-05-08 13:01:01 +0900

    fix : 페이지 선택시 상단으로 view 이동

--- src/@components/AnswerListPage/AnswerPaging.tsx
+++ src/@components/AnswerListPage/AnswerPaging.tsx
@@ -1,32 +1,47 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { useRecoilValue } from 'recoil';
+import styled from 'styled-components';
 import { AnswerCard, Paging } from '.';
 import useGetAnswerList from '../../lib/hooks/useGetAnswerList';
 import { problemInfoState } from '../../recoil/atom/problemInfo';
 import { Error404 } from '../@common';
 
-const AnswerPaging = () => {
+interface AnswerPagingProps {
+  handleScrollToTop: () => void;
+}
+const AnswerPaging = (props: AnswerPagingProps) => {
+  const { handleScrollToTop } = props;
+  const scrollRef = useRef<HTMLDivElement>(null);
+
   const { problemId } = useRecoilValue(problemInfoState);
-  const [size, setSize] = useState(0);
-  const { problemAnswerInfo, isLoading, isError } = useGetAnswerList(problemId, size);
+  const { problemAnswerInfo, isLoading, isError, setSize, size } = useGetAnswerList(problemId);
 
   const handleClickPage = (newSize: number) => {
-    setSize(newSize - 1);
+    handleScrollToTop();
+    setSize(newSize);
   };
 
   if (problemAnswerInfo) {
     const { replyData, totalCount } = problemAnswerInfo;
-
     return (
-      <>
+      <St.AnswerPagingWrapper ref={scrollRef}>
         {replyData.map(({ replyId, userId, name, imageUrl, answer }, idx) => (
           <AnswerCard key={`${name}-${idx}`} replyId={replyId} userId={userId} name={name} imageUrl={imageUrl} answer={answer} />
         ))}
         <Paging totalItemsCount={totalCount} activePage={size} handleClickPage={handleClickPage} />
-      </>
+      </St.AnswerPagingWrapper>
     );
   }
   return <Error404 />;
 };
 
 export default AnswerPaging;
+
+const St = {
+  AnswerPagingWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    width: 100%;
+  `,
+};
