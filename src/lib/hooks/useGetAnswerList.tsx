653d45b3fb5f01711ed60e8c814c3a7ecc2df8aa
seohee0112 <ghdtj7@naver.com>
2023-05-08 13:00:36 +0900

    fix : useSWRInfinite로 페이지 불러오기 변경

--- src/lib/hooks/useGetAnswerList.tsx
+++ src/lib/hooks/useGetAnswerList.tsx
@@ -1,13 +1,14 @@
 import useSWRInfinite from 'swr/infinite';
-import { ProblemAnswerInfo } from '../../type/history';
+import { GetProblemAnswerInfo, ProblemAnswerInfo } from '../../type/history';
 
 import { peerNaGetFetcher } from '../axios';
 
 const useGetAnswerList = (problemId: number) => {
-  const { data, isLoading, error, size, setSize } = useSWRInfinite<ProblemAnswerInfo[]>(
-    (idx: number, answerList: ProblemAnswerInfo[]) => {
+  const { data, isLoading, error, size, setSize } = useSWRInfinite<GetProblemAnswerInfo>(
+    (idx: number, answerList: GetProblemAnswerInfo) => {
       if (!idx) return `api/problems/replies?problemId=${problemId}&page=0`;
-      if (answerList[0]) return `api/problems/replies?problemId=${problemId}&page=${idx}`;
+
+      if (answerList.replyData[0]) return `api/problems/replies?problemId=${problemId}&page=${idx}`;
       return null;
     },
     peerNaGetFetcher,
@@ -15,9 +16,8 @@ const useGetAnswerList = (problemId: number) => {
       errorRetryCount: 1,
     },
   );
-  const parseResultList = data?.flat();
   return {
-    answerList: parseResultList?.splice(0, 16),
+    problemAnswerInfo: data ? data[size - 1] : { replyData: [], totalCount: 0 },
     isLoading,
     isError: error,
     size,
