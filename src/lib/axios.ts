5dba30830120cd8cba9941395e679346b7de9100
seohee0112 <ghdtj7@naver.com>
2023-05-08 11:30:08 +0900

    feat : axios 인스턴스에 polling 로직구현

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,11 @@
+import { PeerMatchInfo } from './../type/problem';
 import axios from 'axios';
 
+const sleep = (timeout: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+};
 const peerNaClient = axios.create({
   baseURL: process.env.REACT_APP_IP,
   headers: {
@@ -12,25 +18,40 @@ peerNaClient.interceptors.request.use((config: any) => {
   const headers = {
     ...config.headers,
   };
-
   return { ...config, headers };
 });
 
 peerNaClient.interceptors.response.use(
-  function (response) {
+  (response) => {
+    const polling = async () => {
+      let pollingRes = await axios.get<PeerMatchInfo>(`${process.env.REACT_APP_IP}api/match`, { withCredentials: true });
+      while (pollingRes.status === 202) {
+        await sleep(2000);
+        try {
+          pollingRes = await axios.get<PeerMatchInfo>(`${process.env.REACT_APP_IP}api/match`, { withCredentials: true });
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
+      return pollingRes;
+    };
+    if (response.status === 202) {
+      return polling();
+    }
+
     return response;
   },
   async (error) => {
-    const {
-      config,
-      response: { status },
-    } = error;
+    const { config, response } = error;
     const originalRequest = config;
 
-    if (status === 401) {
-      window.location.href = `${process.env.REACT_APP_REDIRECT_URL}`;
+    if (response) {
+      if (response.status === 401) {
+        window.location.href = `${process.env.REACT_APP_REDIRECT_URL}`;
 
-      return axios(originalRequest);
+        return axios(originalRequest);
+      }
     }
     return error.response;
   },
