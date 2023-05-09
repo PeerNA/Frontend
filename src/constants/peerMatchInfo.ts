d9cd0163e2ca65b0232cae3886f96ed18d1aa123
seohee0112 <ghdtj7@naver.com>
2023-05-09 18:14:40 +0900

    docs : 동료매칭화면에서 필요한 상수 정의

---
+++ src/constants/peerMatchInfo.ts
@@ -0,0 +1,9 @@
+export enum PEER_MATCH_MODAL_TYPE {
+  SUBMIT_ANSWER = 0,
+  NEXT_QUESTION = 1,
+}
+
+export const PEER_MATCH_MODAL_INFO = [
+  { type: '답안 제출', content: '나의 답안을 제출하시겠습니까?' },
+  { type: '다음 문제', content: '다음문제로 넘어가시겠습니까?' },
+];
