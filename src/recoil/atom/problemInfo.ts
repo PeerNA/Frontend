767054efe0be623e13814c7fc4c0f200f07eba87
seohee0112 <ghdtj7@naver.com>
2023-05-09 18:12:43 +0900

    feat : 동료 매칭 화면에 필요한 atom, 답안제출 atom 구현

--- src/recoil/atom/problemInfo.ts
+++ src/recoil/atom/problemInfo.ts
@@ -1,10 +1,12 @@
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
-import { ProblemInfo } from '../../type/problem';
+import { PeerMatchInfo, PostReplyInfo, ProblemInfo } from '../../type/problem';
 
 enum StateType {
   PROBLEM_INFO = 'problemInfo',
   ANSWER_INFO_MODAL = 'answerInfo',
+  PEER_MATCH_INFO = 'peerMatchIngo',
+  REPLY_ANSWER = 'replyAnswer',
 }
 
 const { persistAtom } = recoilPersist();
@@ -14,6 +16,37 @@ export const problemInfoState = atom<ProblemInfo>({
   default: {
     problemId: 0,
     question: '',
+  },
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const peerMatchInfoState = atom<PeerMatchInfo>({
+  key: StateType.PEER_MATCH_INFO,
+  default: {
+    roomId: 0,
+    historyId: 0,
+    problem: {
+      id: 0,
+      category: '',
+      question: '',
+      answer: '',
+    },
+    peer: {
+      id: 0,
+      imageUrl: '',
+      name: '',
+    },
+    isAnswerSubmit: false,
+  },
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const replyAnswerInfoState = atom<PostReplyInfo>({
+  key: StateType.REPLY_ANSWER,
+  default: {
+    answer: '',
+    problemId: 0,
+    historyId: 0,
   },
   effects_UNSTABLE: [persistAtom],
 });
