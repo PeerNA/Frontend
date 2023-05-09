535c4d27597f146c0636adf49e0c92df7e54a2ef
seohee0112 <ghdtj7@naver.com>
2023-05-09 18:16:28 +0900

    docs : 동료답안 잠금화면 내보내기 구현

--- src/@components/PeerMatchingPage/index.ts
+++ src/@components/PeerMatchingPage/index.ts
@@ -1,3 +1,4 @@
 export { default as ProblemSolving } from './ProblemSolving';
 export { default as ChatingRoom } from './ChatingRoom';
 export { default as TimeStatusBar } from './TimeStatusBar';
+export { default as LockSolving } from './LockSolving';
