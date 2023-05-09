10abefcba347a980b688d0e868984064895fa385
seohee0112 <ghdtj7@naver.com>
2023-05-09 18:16:11 +0900

    feat : 동료답안 잠금 화면 컴포넌트 구현

---
+++ src/@components/PeerMatchingPage/LockSolving.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LockSolving = () => {
+  return (
+    <St.LockSolving>
+      <span className="material-symbols-outlined">lock</span>
+    </St.LockSolving>
+  );
+};
+
+export default LockSolving;
+
+const St = {
+  LockSolving: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 47rem;
+    border: 0.2rem solid ${({ theme }) => theme.colors.Peer_Color_Blue};
+
+    > span {
+      font-size: 10rem;
+      color: ${({ theme }) => theme.colors.Peer_Color_Sky_1};
+    }
+  `,
+};
