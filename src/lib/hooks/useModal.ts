8f18a52f6ce1e4bd28e81ffc1c777b296430ed91
seohee0112 <ghdtj7@naver.com>
2023-05-12 20:22:12 +0900

    fix : 동료매칭 문제풀이 관련 토글 함수 구현

--- src/lib/hooks/useModal.ts
+++ src/lib/hooks/useModal.ts
@@ -1,17 +1,20 @@
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useResetRecoilState } from 'recoil';
 import { useCallback } from 'react';
 import { modalInfoState } from '../../recoil/atom/profileBar';
 
 const useModal = () => {
   const [modalInfo, setModalInfo] = useRecoilState(modalInfoState);
-  const { isPeernaModal, isProfileModal } = modalInfo;
+  const { isPeernaModal, isProfileModal, isPeerMatchModal } = modalInfo;
 
   const toggleModal = (isProfileType: boolean, isSuccess?: boolean) => {
-    if (isProfileType) setModalInfo({ isPeernaModal, isProfileModal: !isProfileModal });
-    else setModalInfo({ isPeernaModal: isSuccess ? false : !isPeernaModal, isProfileModal });
+    if (isProfileType) setModalInfo({ isPeernaModal, isProfileModal: !isProfileModal, isPeerMatchModal });
+    else setModalInfo({ isPeernaModal: isSuccess ? false : !isPeernaModal, isProfileModal, isPeerMatchModal });
   };
 
-  return { isPeernaModal, isProfileModal, toggleModal };
+  const togglePeerMatchModal = () => {
+    setModalInfo({ isPeerMatchModal: !isPeerMatchModal, isPeernaModal, isProfileModal });
+  };
+  return { isPeernaModal, isProfileModal, isPeerMatchModal, toggleModal, togglePeerMatchModal };
 };
 
 export default useModal;
