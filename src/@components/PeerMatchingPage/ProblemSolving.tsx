aa69cff7b1b53f751edcb78b9e1291b8554c1b95
seohee0112 <ghdtj7@naver.com>
2023-05-12 20:20:34 +0900

    feat : 상호 동료매칭후 문제풀이 기능 구현

--- src/@components/PeerMatchingPage/ProblemSolving.tsx
+++ src/@components/PeerMatchingPage/ProblemSolving.tsx
@@ -1,41 +1,60 @@
 import { useRef } from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue, useResetRecoilState } from 'recoil';
 import styled from 'styled-components';
-import { PEER_MATCH_MODAL_INFO, PEER_MATCH_MODAL_TYPE } from '../../constants/peerMatchInfo';
-import { postReplyAnswerData } from '../../lib/api/problem';
+import { PEER_MATCH_MODAL_INFO, PEER_MATCH_MODAL_TYPE, USER_TYPE } from '../../constants/peerMatchInfo';
+import { getNextPeerMatch, getPeerReplySubmit, postReplyAnswerData } from '../../lib/api/problem';
 import useModal from '../../lib/hooks/useModal';
 import ModalPortal from '../../ModalPortals';
-import { peerMatchInfoState, replyAnswerInfoState } from '../../recoil/atom/problemInfo';
+import { peerMatchAnswerInfoState, peerMatchInfoState, replyAnswerInfoState } from '../../recoil/atom/problemInfo';
 import { userInfoState } from '../../recoil/atom/userInfo';
 import { LearningBtnList, PeerNaBtn, PeerNaModal, QuestionTitle, UserInputBox } from '../@common';
 import LockSolving from './LockSolving';
 import TimeStatusBar from './TimeStatusBar';
 
 const ProblemSolving = () => {
-  const { isPeernaModal, toggleModal } = useModal();
+  const { isPeerMatchModal, togglePeerMatchModal } = useModal();
   const [replyAnswerInfo, setReplyAnswerInfo] = useRecoilState(replyAnswerInfoState);
   const modalContentRef = useRef(PEER_MATCH_MODAL_INFO[PEER_MATCH_MODAL_TYPE.SUBMIT_ANSWER]);
-  const peerMatchInfo = useRecoilValue(peerMatchInfoState);
+  const [peerMatchInfo, setPeerMatchInfo] = useRecoilState(peerMatchInfoState);
+  const [peerMatchAnswerInfo, setPeerMatchAnswerInfo] = useRecoilState(peerMatchAnswerInfoState);
+  const { keyword, userInfo } = peerMatchAnswerInfo;
+
   const myInfo = useRecoilValue(userInfoState);
   const {
+    roomId,
     problem: { question },
     peer,
     isAnswerSubmit,
   } = peerMatchInfo;
+  const { isTimeRemain } = isAnswerSubmit;
 
   const handleAnswerSubmit = () => {
-    toggleModal(false);
+    if (!isAnswerSubmit.isMyAnswer) togglePeerMatchModal();
   };
 
   const handleNextQuestion = () => {
     modalContentRef.current = PEER_MATCH_MODAL_INFO[PEER_MATCH_MODAL_TYPE.NEXT_QUESTION];
+    togglePeerMatchModal();
   };
-  const postReplyAnswer = () => {
-    // postReplyAnswerData(replyAnswerInfo);
-    console.log('보내기');
+  const postReplyAnswer = async () => {
+    //문제풀이 완료후 답안제출 확인
+    const data = await postReplyAnswerData(replyAnswerInfo);
+
+    if (data?.status === 200) {
+      // 시간 남아있으면 이제 동료 검사
+      if (isTimeRemain) {
+        const data = await getPeerReplySubmit(roomId);
+
+        if (data) {
+          setPeerMatchAnswerInfo(data);
+          setPeerMatchInfo({ ...peerMatchInfo, isAnswerSubmit: { ...isAnswerSubmit, isMyAnswer: true, isPeerAnswer: true, isTimeRemain: false } });
+        }
+      } else setPeerMatchInfo({ ...peerMatchInfo, isAnswerSubmit: { ...isAnswerSubmit, isMyAnswer: true } });
+    }
   };
-  const getNextQuestion = () => {
-    console.log('다음문제');
+
+  const getNextQuestion = async () => {
+    const data = await getNextPeerMatch(roomId, peer.id);
   };
 
   const handleAnswerTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -45,29 +64,39 @@ const ProblemSolving = () => {
   return (
     <>
       <St.PeerMatchingWrapper>
-        <LearningBtnList isActive={isAnswerSubmit} />
-        <QuestionTitle question={question} isAnswer={isAnswerSubmit} keywordList={[]} />
+        <LearningBtnList isActive={isAnswerSubmit.isMyAnswer} />
+        <QuestionTitle question={question} isAnswer={isAnswerSubmit.isMyAnswer} keywordList={keyword} />
         <TimeStatusBar />
         <St.UserInputBoxWrapper>
           <div>
-            <UserInputBox isModify={!isAnswerSubmit} userName={myInfo.name} imageUrl={myInfo.imageUrl} handleAnswerTextArea={handleAnswerTextArea} />
+            <UserInputBox
+              isModify={!isAnswerSubmit.isMyAnswer}
+              userName={myInfo.name}
+              imageUrl={myInfo.imageUrl}
+              textAreaValue={replyAnswerInfo.answer}
+              handleAnswerTextArea={handleAnswerTextArea}
+            />
             <PeerNaBtn
-              isActive={!isAnswerSubmit && Boolean(replyAnswerInfo.answer.length)}
+              isActive={!isAnswerSubmit.isMyAnswer && Boolean(replyAnswerInfo.answer.length)}
               content={PEER_MATCH_MODAL_INFO[PEER_MATCH_MODAL_TYPE.SUBMIT_ANSWER].type}
               handleBtnClick={handleAnswerSubmit}
             />
           </div>
           <div>
-            {!isAnswerSubmit ? <LockSolving /> : <UserInputBox isModify={!isAnswerSubmit} userName={peer.name} imageUrl={peer.imageUrl} />}
+            {!isAnswerSubmit.isPeerAnswer ? (
+              <LockSolving />
+            ) : (
+              <UserInputBox isModify={!isAnswerSubmit} userName={peer.name} imageUrl={peer.imageUrl} content={userInfo[USER_TYPE.PEER].answer} />
+            )}
             <PeerNaBtn
-              isActive={isAnswerSubmit}
+              isActive={isAnswerSubmit.isMyAnswer && isAnswerSubmit.isPeerAnswer}
               content={PEER_MATCH_MODAL_INFO[PEER_MATCH_MODAL_TYPE.NEXT_QUESTION].type}
               handleBtnClick={handleNextQuestion}
             />
           </div>
         </St.UserInputBoxWrapper>
       </St.PeerMatchingWrapper>
-      {isPeernaModal && (
+      {isPeerMatchModal && (
         <ModalPortal>
           <PeerNaModal
             modalContent={modalContentRef.current.content}
