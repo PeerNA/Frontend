b8d1c04bd078fe0e7cdbc9bd72086ce4ed72de42
seohee0112 <ghdtj7@naver.com>
2023-05-12 20:23:16 +0900

    feat : 답안 제출여부만 가져오는 셀렉터 구현

--- src/recoil/selector/problemInfo.ts
+++ src/recoil/selector/problemInfo.ts
@@ -1,8 +1,20 @@
 import { peerMatchInfoState } from './../atom/problemInfo';
 import { selector } from 'recoil';
 import { convertLetterToTime } from '../../lib/utils/problemInfo';
+import { AnswerSubmitInfo } from '../../type/problem';
 
 export const calculateTime = selector({
   key: 'timeSelector',
   get: ({ get }) => convertLetterToTime(get(peerMatchInfoState).problem.question.length),
+});
+
+export const answerSubmitInfo = selector({
+  key: 'answerSubmit',
+  get: ({ get }) => get(peerMatchInfoState).isAnswerSubmit,
+
+  set: ({ set, get }, newValue) => {
+    const peerMatchInfo = get(peerMatchInfoState);
+
+    return set(peerMatchInfoState, { ...peerMatchInfo, isAnswerSubmit: { isTimeRemain: false, isMyAnswer: true, isPeerAnswer: true } });
+  },
 });
