2cf580960d38e5e9df4d53160a811c2246254364
seohee0112 <ghdtj7@naver.com>
2023-05-15 15:29:02 +0900

    docs : 동료 기다리기 상수 추가

--- src/constants/peerMatchInfo.ts
+++ src/constants/peerMatchInfo.ts
@@ -1,11 +1,13 @@
 export enum PEER_MATCH_MODAL_TYPE {
   SUBMIT_ANSWER = 0,
   NEXT_QUESTION = 1,
+  WAIT_PEER = 2,
 }
 
 export const PEER_MATCH_MODAL_INFO = [
   { type: '답안 제출', content: '나의 답안을 제출하시겠습니까?' },
   { type: '다음 문제', content: '다음문제로 넘어가시겠습니까?' },
+  { type: '동료 기다리기', content: '동료를 기다리는 중입니다.' },
 ];
 export enum USER_TYPE {
   MY = 0,
