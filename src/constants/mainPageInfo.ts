8ea18062af36be59dff70d445764a5b035aee2ec
seohee0112 <ghdtj7@naver.com>
2023-05-28 17:20:45 +0900

    docs : ts로 확장자 변경

--- src/constants/mainPageInfo.ts
+++ src/constants/mainPageInfo.ts
@@ -25,7 +25,15 @@ export const SUBJECT_CATEGORY_LIST = [
   'TECH',
   'CODING_TEST',
 ];
-export const CAREER_TYPE_LIST = ['3년이상', '1년이상', '1년이하'];
-export const SELET_TITLE_LIST = ['학습경력', '1순위', '2순위', '3순위'];
-
+// export const CAREER_TYPE_LIST = ['3년이상', '1년이상', '1년이하'];
+export const SELET_TITLE_LIST = ['1순위', '2순위', '3순위'];
 export const POST_CAREER_TYPE_LIST = ['OVER_3', 'OVER_1', 'UNDER_1'];
+
+export enum SCORE_LEVEL {
+  LEVEL_1 = 0,
+  LEVEL_2 = 1,
+  LEVEL_3 = 2,
+  LEVEL_4 = 3,
+  LEVEL_5 = 4,
+  LEVEL_6 = 5,
+}
